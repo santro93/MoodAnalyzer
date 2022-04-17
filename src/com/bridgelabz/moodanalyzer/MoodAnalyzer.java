6b107a28a1fd4b13cbe1627299493a5da4b78fcf
santro93 <nsantosh435@gmail.com>
2022-04-17 17:42:42 +0530

    Analyse And Respond Happy or Sad Mood

--- src/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
+++ src/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
@@ -3,5 +3,19 @@ package com.bridgelabz.moodanalyzer;
 public class MoodAnalyzer {
 	public static void main(String[] args) {
 		System.out.println("Welcome To Mood Analyser Program!");
+
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+		String message = moodAnalyzer.analyseMood("This is Happy message");
+		System.out.println(message);
+		message = moodAnalyzer.analyseMood("This is Sad message");
+		System.out.println(message);
+	}
+
+	private String analyseMood(String message) {
+		if (message.contains("Sad")) {
+			return "Sad";
+		} else {
+			return "Happy";
+		}
 	}
 }
